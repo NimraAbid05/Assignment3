bf26181a636744d095c9c4aeb4f30dfa07769ef8
NimraAbid05 <nimram214@gmail.com>
2022-10-30 03:09:15 -0700

    list

---
+++ task1.dart
@@ -0,0 +1,12 @@
+void main() {
+  //Q.1: Create a list of names and print all names using list.
+  List n = ["Ali", "Sana", "Laiba", "Usman"];
+
+  //print list
+  print(n);
+  //print list using index
+  print(n[0]);
+  print(n[1]);
+  print(n[2]);
+  print(n[3]);
+}
---
+++ task3.dart
@@ -0,0 +1,10 @@
+void main() {
+  //Q3.Add your 7 friend names to the list.
+  // Use where to find a day name that starts with alphabet a.
+  List n = [];
+  n.addAll(["Asma", "Nadia", "Maha", "Aliya", "Ayesha", "Maheen", "Maha"]);
+  print(n);
+  print("Names start with Alphabet A");
+  n.where((a) => true);
+  print(n);
+}
