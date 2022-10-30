27759dd58134aeab8ae780cc619eadde5cd59125
NimraAbid05 <nimram214@gmail.com>
2022-10-30 03:30:10 -0700

    map

--- task5.dart
+++ task5.dart
@@ -1,3 +1,7 @@
-void main(){
+void main() {
+  //Q.5 Create a map with name, phone keys and store some values to it.
+  // Use where to find all keys that have length 4
+  Map n = {"Name": "Nimra", "Phone": 030045990};
+  print(n);
   
 }
