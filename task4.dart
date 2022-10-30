34e1ead2a6a7d0cbf3092959bd07bdbfdddff1f6
NimraAbid05 <nimram214@gmail.com>
2022-10-30 03:21:15 -0700

    map

---
+++ task4.dart
@@ -0,0 +1,15 @@
+void main() {
+  //Q.4 Create a map with name, address, age, country keys
+  //and store values to it.
+  // Update country name to other country and print all keys and values.
+  Map m = {
+    "Name": "Nimra",
+    "Address": "Karachi",
+    "Age": 21,
+    "Country": "Pakistan"
+  };
+  print(m);
+  //update country name
+  m["Country"] = "Srilanka";
+  print(m);
+}
